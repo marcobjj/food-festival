f79f6b4f69d9fee7f9561602bdd615b95f5dfa66
Marco Evangelista <marcobjj@users.noreply.github.com>
2020-11-30 22:15:57 -0800

    adds file loder, image webpack loader

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,29 @@ module.exports = {entry: {
     path: path.resolve(__dirname, 'dist'),
     filename: "[name].bundle.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/i,
+        use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name (file) {
+                  return "[path][name].[ext]"
+                },
+                publicPath: function(url) {
+                  return url.replace("../", "/assets/")
+                }
+              } 
+            },
+            {
+              loader: 'image-webpack-loader'
+            }
+          ]
+      }
+    ]
+  },
   plugins: [ new webpack.ProvidePlugin({
     $: "jquery",
     jQuery: "jquery"
